f67e01ab34983c548d11e5b811241a22144ea390
JIHO <92428617+IJHO-NUl1l1@users.noreply.github.com>
2024-08-06 12:24:28 +0000

    mission1

--- src/mission1/transaction.js
+++ src/mission1/transaction.js
@@ -1,24 +1,31 @@
 class Transaction {
   constructor(inputUTXOs, outputUTXOs) {
-    this.transaction = { inputUTXOs, outputUTXOs };
+    this.transactions = { inputUTXOs, outputUTXOs }
   }
   execute() {
-    const isDoubleSpend = this.transaction.inputUTXOs.findIndex(
-      (utxo) => utxo.spent == true
-    );
+    const isDoubleSpend = this.transactions.inputUTXOs.findIndex(
+      (utxo) => utxo.spent === true
+    )
 
     if (isDoubleSpend !== -1) {
-      throw new Error("input TXO is already spent.");
+      throw new Error('input TXO is already spent.')
     }
 
-    /**
-     * 📚 Mission 1.
-     * 입력 UTXO의 총값이 출력 UTXO의 총값을 커버할 만큼 충분한지 확인한다.
-     * 입력의 총값이 출력의 총값보다 작으면 execute 함수에서 에러를 던진다.
-     */
+    const totalInputAmount = this.transactions.inputUTXOs.reduce(
+      (acc, cur) => acc + cur.amount,
+      0
+    )
+    const totalOutputAmount = this.transactions.outputUTXOs.reduce(
+      (acc, cur) => acc + cur.amount,
+      0
+    )
+
+    if (totalInputAmount < totalOutputAmount) {
+      throw new Error(
+        'insufficient amount: total value of the inputs is less than the total value of the outputs!'
+      )
+    }
   }
 }
 
-module.exports = Transaction;
-
-//test
+module.exports = Transaction
